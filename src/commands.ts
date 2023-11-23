264ff49f1695d4bc38da399f55085b195eb51b40
Minh Tung Nguyen <minhtung04042001@gmail.com>
2023-11-23 23:45:52 +0100

    add initialization

--- src/commands.ts
+++ src/commands.ts
@@ -6,6 +6,7 @@ const initialCommandMap: { [key: string]: string } = {
   "\\definition": `>[!math|{"type":"definition","number":"auto","setAsNoteMathLink":false}] Definition. `,
   "\\theorem": `>[!math|{"type":"theorem","number":"auto","setAsNoteMathLink":false}] Theorem. `,
   "\\proposition": `>[!math|{"type":"proposition","number":"auto","setAsNoteMathLink":false}] Proposition. `,
+  "\\lemma": `>[!math|{"type":"lemma","number":"auto","setAsNoteMathLink":false}] Lemma. `,
 
   // other commands
   "\\example": `>[!example]`,
@@ -14,6 +15,7 @@ const initialCommandMap: { [key: string]: string } = {
 const commandMap: { [key: string]: string } = {
   ...initialCommandMap,
   // Algorithm commands
+  "\\initialization": ">[!init] **Initialization:** ",
   "\\input": ">[!input] **Input:** ",
   "\\output": ">[!output] **Output:** ",
   "\\ensure": ">[!ensure] **Ensure:** ",
